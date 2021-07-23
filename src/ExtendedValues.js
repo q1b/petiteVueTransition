c32f03a95df134e00ec9334df5dd151f896f315d
sacarvy <69889565+sacarvy@users.noreply.github.com>
2021-07-23 23:35:28 +0530

    Add files via upload

---
+++ src/ExtendedValues.js
@@ -0,0 +1,30 @@
+const ValuesArray = [
+	['h','height'],
+	['bg','background'],
+	['p', 'padding'],
+	['m', 'margin'],
+	['t', 'top'],
+	['r','right'],
+	['l','left'],
+	['t','top'],
+];
+export const shortProp=['h','bg','p','m','t','r','l','t'];
+const extended = new Map(ValuesArray);
+// to convert pt to padding-top
+export const extValues = (propsStr,joiningValue) => {
+	// a is like 'pt' or 'p'
+	if(propsStr.length===1){
+		// check it is 'p'
+		return extended.get(propsStr)
+	}else{
+		// if it is 'pt'
+		let propsArray = propsStr.split('') // ['p','t']
+		let extendedArray = []
+		propsArray.forEach((props)=>{
+			// getting 'p' then converting it to extended form which is padding
+			extendedArray = [...extendedArray, extended.get(props)]
+		})
+		// returning padding-top here,
+		return extendedArray.join(joiningValue)
+	}
+}
